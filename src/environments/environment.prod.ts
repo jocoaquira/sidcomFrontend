dcf850e64b6c1b4b94d9492de91b37ac34c0c108
Gobierno Autonomo Oruro <gobiernooruro2024@gmail.com>
2025-03-17 08:42:22 -0400

    fix environment.prod, environment API https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   FRONTEND_URL:'https://sidcomv2.min.oruro.gob.bo/',
   //FRONTEND_URL:'https://mineria.oruro.gob.bo/sidcom/',
    // API_URL:'http://localhost/sidcom_v2/sidcom_backend/public/api/'
- API_URL: 'http://sidcomv2api.min.oruro.gob.bo'
+ API_URL: 'https://sidcomv2api.min.oruro.gob.bo'
 };
