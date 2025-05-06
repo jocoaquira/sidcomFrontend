e871fcccf53914f3034d3c1063a3a9ee52339096
jocrock <jocrock.cga@gmail.com>
2025-05-06 12:44:06 -0400

    actservidor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,18 @@
+
 export const environment = {
-  production: true,
+    production: true,
+    FRONTEND_URL:'https://sidcom2.oruro.gob.bo/',
+    //FRONTEND_URL:'http://192.168.205.73:4200/',
+    //API_URL:'http://localhost/sidcom_v2/sidcom_backend/public/api/'
 
-  FRONTEND_URL:'https://sidcomv2.min.oruro.gob.bo/',
-   // API_URL:'http://localhost/sidcom_v2/sidcom_backend/public/api/'
- API_URL: 'https://sidcomv2api.min.oruro.gob.bo/',
-  //FRONTEND_URL:'https://mineria.oruro.gob.bo/sidcom/',
-   // API_URL:'http://localhost/sidcom_v2/sidcom_backend/public/api/'
+   //API_URL: 'http://192.168.100.188:5000/',
+   API_URL: 'https://apim.oruro.gob.bo/',
+   //API_URL: 'http://192.168.205.73:4201/',
 
-};
+
+   //API_URL: 'http://sidcomv2api.min.oruro.gob.bo/',
+
+   DIAS_ANULACION:4,
+   TIEMPO_AUTH:30,
+    //API_URL: 'http://192.168.1.13:5000/'
+  };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,19 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+
 
 export const environment = {
-  production: false,
-  FRONTEND_URL:'https://sidcomv2.min.oruro.gob.bo/',
-  //FRONTEND_URL:'http://192.168.205.73:4200/',
-  //API_URL:'http://localhost/sidcom_v2/sidcom_backend/public/api/'
+    production: false,
+    FRONTEND_URL:'https://sidcom2.oruro.gob.bo/',
+    //FRONTEND_URL:'http://192.168.205.73:4200/',
+    //API_URL:'http://localhost/sidcom_v2/sidcom_backend/public/api/'
 
- API_URL: 'http://127.0.0.1:30002/',
- //API_URL: 'https://sidcomv2api.min.oruro.gob.bo/',
- //API_URL: 'https://sidcom2backend.onrender.com/',
+   //API_URL: 'http://192.168.100.188:5000/',
+   API_URL: 'https://apim.oruro.gob.bo/',
+   //API_URL: 'http://192.168.205.73:4201/',
 
 
- //API_URL: 'http://sidcomv2api.min.oruro.gob.bo/',
+   //API_URL: 'http://sidcomv2api.min.oruro.gob.bo/',
 
- DIAS_ANULACION:4,
- TIEMPO_AUTH:30,
-  //API_URL: 'http://192.168.1.13:5000/'
-};
+   DIAS_ANULACION:4,
+   TIEMPO_AUTH:30,
+    //API_URL: 'http://192.168.1.13:5000/'
+  };
