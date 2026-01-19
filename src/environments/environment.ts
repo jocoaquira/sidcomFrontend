adc3f093a378fa5e5c08fa0e173337cfe1238f25
jocoaquira <jocoqquira@gmail.com>
2026-01-19 10:44:39 -0400

    servidor casa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,16 +2,17 @@
 
 export const environment = {
     production: false,
-    FRONTEND_URL:'https://sidcom2.oruro.gob.bo/',
+    FRONTEND_URL:'https://sidcom.jocrock.lat/',
+    //FRONTEND_URL:'https://sidcom2.oruro.gob.bo/',
     //FRONTEND_URL:'http://192.168.205.73:4200/',
     //API_URL:'http://localhost/sidcom_v2/sidcom_backend/public/api/'
 
-    API_URL: 'http://127.0.0.1:80/',
-    API_WS: 'ws://127.0.0.1:80/',
+    //API_URL: 'http://127.0.0.1:80/',
+    //API_WS: 'ws://127.0.0.1:80/',
    // API_URL: 'http://localhost:80/',
    // API_WS: 'ws://localhost:80/',
-    //API_URL: 'https://apimin.jocrock.lat/',
-    //API_WS: 'wss://apimin.jocrock.lat/',
+    API_URL: 'https://apimin.jocrock.lat/',
+    API_WS: 'wss://apimin.jocrock.lat/',
    //API_URL: 'http://192.168.205.129:3000/',
    //API_WS: 'ws://192.168.205.129:3000/',
    //API_URL: 'https://apim.oruro.gob.bo/',
