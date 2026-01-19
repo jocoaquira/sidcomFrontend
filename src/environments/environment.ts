96ad276d1f8d2602f819f2903d913731bbd83496
jocoaquira <jocoqquira@gmail.com>
2026-01-19 16:10:50 -0400

    todo listo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@
 
 export const environment = {
     production: false,
-    FRONTEND_URL:'https://sidcom.jocrock.lat/',
-    //FRONTEND_URL:'https://sidcom2.oruro.gob.bo/',
+    //FRONTEND_URL:'https://sidcom.jocrock.lat/',
+    FRONTEND_URL:'https://sidcom2.oruro.gob.bo/',
     //FRONTEND_URL:'http://192.168.205.73:4200/',
     //API_URL:'http://localhost/sidcom_v2/sidcom_backend/public/api/'
 
@@ -11,12 +11,12 @@ export const environment = {
     //API_WS: 'ws://127.0.0.1:80/',
    // API_URL: 'http://localhost:80/',
    // API_WS: 'ws://localhost:80/',
-    API_URL: 'https://apimin.jocrock.lat/',
-    API_WS: 'wss://apimin.jocrock.lat/',
+   // API_URL: 'https://apimin.jocrock.lat/',
+   // API_WS: 'wss://apimin.jocrock.lat/',
    //API_URL: 'http://192.168.205.129:3000/',
    //API_WS: 'ws://192.168.205.129:3000/',
-   //API_URL: 'https://apim.oruro.gob.bo/',
-   //API_WS: 'wss://apim.oruro.gob.bo/',
+   API_URL: 'https://apim.oruro.gob.bo/',
+   API_WS: 'wss://apim.oruro.gob.bo/',
    //API_URL: 'http://192.168.205.73:4201/',
 
 
